68a0705114283bd0091f4b6bc260badca3a951bf
shumrussia <azat.nuriakhmetov@gmail.com>
2021-12-30 12:02:45 +0300

    created rest endpoint for new note

--- src/main/java/ru/azatnuriakhmetov/springboot/entity/Note.java
+++ src/main/java/ru/azatnuriakhmetov/springboot/entity/Note.java
@@ -1,10 +1,16 @@
 package ru.azatnuriakhmetov.springboot.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -16,8 +22,19 @@ public class Note {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String title;
+	
 	private String body; 
+	
 	private String category;
+	
+	@Column(name="created_at", nullable=false, updatable=false)
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@Column(name="updated_at")
+	@UpdateTimestamp
+	private Date updatedAt;
 	
 }
